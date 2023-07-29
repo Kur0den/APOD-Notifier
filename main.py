fcdcd43eacd189fd0c5e284419e6a59a419a0012
Kur0den0010 <69957747+Kur0den@users.noreply.github.com>
2023-07-29 09:22:49 +0900

    ほとんど完成

--- main.py
+++ main.py
@@ -1,31 +1,38 @@
 import time
+from html.parser import HTMLParser
+from os import environ as env
 
 import schedule
+from dotenv import load_dotenv
+from misskey import Misskey
 from requests import get
 from xmltodict import parse
 
+load_dotenv()
+misskey_domain = "koliosky.com"
+
+mk = Misskey(misskey_domain, i = env["MISSKEY_TOKEN"])
+
+class MyParser(HTMLParser):
+    def handle_data(self, data):
+        self.desc = data  # データを呼び出すときに呼ばれる
 
 def job():
-    rss_url = "RSS_URL" # RSSのURL
+    print('発火')
+    rss_url = "https://apod.nasa.gov/apod.rss" # RSSのURL
     response = get(rss_url) # RSSを取得
     parse_data = parse(response.text) # RSS(xml)をDict化
 
-    '''
-    ここでparse_dataから欲しい情報を引っ張り出して使う
+    parser = MyParser()
+    parser.feed(parse_data['rss']['channel']['item'][1]['description']) # 説明文を取得
 
-    使いにくいのはしらん
-    '''
+    mk.notes_create(f'TEST\n[今日のAPOD]({parse_data["rss"]["channel"]["item"][1]["link"]})\nTitle: {parse_data["rss"]["channel"]["item"][1]["title"]}\nDesc: {parser.desc}') # Misskeyにノートを送信
 
-## 分ごとにRSSを取得したいなら
-# schedule.every(MINUTE).minutes.do(job)
-## 時ごとに取得なら
-# schedule.every(HOUR).hours.do(job)
-## 指定時刻に取得なら
-# schedule.every().day.at("HH:MM", "TIMEZONE").do(job)
+job()
 
-## 詳しくは https://schedule.readthedocs.io/en/stable/index.html
+'''schedule.every().day.at("12:00", "Asia/Tokyo").do(job)
 
 # ずっと実行させるやつ(?)
 while True:
     schedule.run_pending()
-    time.sleep(1)
+    time.sleep(1)'''
