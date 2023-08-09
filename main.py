f59ee7c0b43314a0c42172e718b93599fcb8419c
Kur0den0010 <69957747+Kur0den@users.noreply.github.com>
2023-08-09 18:02:40 +0900

    misskey上でメンションしたら反応するように

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import asyncio
+import json
 import time
 from html.parser import HTMLParser
 from os import environ as env
 
 import schedule
+import websockets
 from dotenv import load_dotenv
 from misskey import Misskey
 from requests import get
@@ -11,8 +14,10 @@ from xmltodict import parse
 load_dotenv()
 misskey_domain = "koliosky.com"
 
-mk = Misskey(misskey_domain, i = env["MISSKEY_TOKEN"])
 
+mk = Misskey(misskey_domain, i = env["MISSKEY_TOKEN"])
+mk_id = mk.i()['id']
+ws_url = f'wss://{misskey_domain}/streaming?i={env["MISSKEY_TOKEN"]}'
 class MyParser(HTMLParser):
     def handle_data(self, data):
         self.desc = data  # データを呼び出すときに呼ばれる
@@ -28,12 +33,32 @@ def job():
 
     mk.notes_create(f'[今日のAPOD]({parse_data["rss"]["channel"]["item"][1]["link"]})\nTitle: {parse_data["rss"]["channel"]["item"][1]["title"]}\nDesc: {parser.desc}') # Misskeyにノートを送信
 
-schedule.every().day.at("12:00", "Asia/Tokyo").do(job)
+schedule.every().day.at("12:00", "Asia/Tokyo").do(job) # 12:00にRSS取得+投稿を実行するようにスケジュールを登録
+
+async def trigger():# スケジュールずっと実行させるやつ
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+async def runner(): # 生存確認用(Misskey上でメンションしたら反応するやつ)
+    asyncio.create_task(trigger()) # スケジュール用の無限ループ実行
+    async with websockets.connect(ws_url) as ws: # type: ignore  ##websocketに接続
+        await ws.send(json.dumps({
+            'type': 'connect',
+            'body': {
+                'channel': 'main',
+                'id': '1'
+            } # mainチャンネルに接続
+        }))
+        while True:
+            msg = json.loads(await ws.recv())
+            if msg['body']['type'] == 'mention': # メンション時に反応
+                note_id = msg['body']['body']['id']
+                user_name = msg['body']['body']['user']['username']
+                user_host = msg['body']['body']['user']['host']
+                mk.notes_create(text=f'@{user_name}@{user_host} Pong!', reply_id=note_id) # Pong!
+
+
 
 print('ready')
-
-# ずっと実行させるやつ(?)
-while True:
-    schedule.run_pending()
-    time.sleep(1)
-
+asyncio.get_event_loop().run_until_complete(runner()) # runner()を実行
